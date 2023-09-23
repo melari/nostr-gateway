103bcc3a1ec08cd60210fb2531d563baea2ebbf9
Caleb Simpson <caleb@simpson.center>
2023-09-23 15:05:34 +0000

    playground

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,45 @@
 import { Router } from "./router";
 import { unwrap } from "./result.ts"
 import { Resolver } from "./resolver.ts";
+const tls = require('node:tls');
+const https = require('node:https');
+const fs = require('node:fs');
 
+const secureContext = {
+    'mydomain.com': tls.createSecureContext({
+        key: "key",
+        cert: "cert",
+        ca: "ca", // this ca property is optional
+    }),
+    'myotherdomain.com': tls.createSecureContext({
+        key: "key",
+        cert: "cert",
+        ca: "ca", // this ca property is optional
+    }),
+}
+
+const options = {
+    /*SNICallback: function (domain: string, cb: any) {
+        console.log(domain);
+        if (secureContext[domain]) {
+            if (cb) {
+                console.log("1");
+                cb(null, secureContext[domain]);
+            } else {
+                console.log("2")
+                // compatibility for older versions of node
+                return secureContext[domain]; 
+            }
+        } else {
+            throw new Error('No keys/certificates for domain requested');
+        }
+    },*/
+   // must list a default key and cert because required by tls.createServer()
+    key: fs.readFileSync("./default-key.pem"), 
+    cert: fs.readFileSync("./default-cert.pem"), 
+}
+
+/*
 const server = Bun.serve({
     port: 3000,
     async fetch(request) {
@@ -22,7 +60,18 @@ const server = Bun.serve({
         }
 
         return new Response(contentEvent.content, { headers: { "Content-Type": "text/html" } });
-    },  
-});
+    },
+    tls: {
+        SNICallback: (domain: string, cb: any) =>  {
+            console.log(domain);
+        },
+        s
+    }
+});*/
+
+const server = https.createServer(options, (req, res) => {
+    res.writeHead(200);
+    res.end('hello world\n');
+}).listen(8080); 
 
 console.log(`Listening on localhost:${server.port}`);
